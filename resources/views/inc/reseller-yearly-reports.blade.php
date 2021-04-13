64caab4a4aebf7e2883c75c244c443d375c78bc5
Miho Daniel <dev.mihodaniel@gmail.com>
2021-04-13 18:49:10 +0200

    Updates styling of yearly report view
    Makes the yearly report view blend in more with the rest of the website

--- resources/views/inc/reseller-yearly-reports.blade.php
+++ resources/views/inc/reseller-yearly-reports.blade.php
@@ -19,84 +19,83 @@
     </div>
     <div class="row">
         <div class="col-12">
-            <div class="card card-body border-bottom border-secondary rounded-0 pr-5 pl-5 pb-3 pt-2 mb-4">
-                <div class="row mb-0 text-center">
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Dátum</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Bevétel</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Kiadás</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Kiszállítások</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Átlag bevétel / Cím</p>
-                    </div>
-                    <div class="col-2">
-                        <p class="font-weight-bold mb-1">Átl. Benzink. / Cím</p>
-                    </div>
+            <div
+                class="row border-bottom mb-0 text-center align-items-end px-3 py-2">
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Dátum</small></p>
+                </div>
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Bevétel</small></p>
+                </div>
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Kiadás</small></p>
+                </div>
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Kiszállítások</small></p>
+                </div>
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Átlag bevétel / Cím</small></p>
+                </div>
+                <div class="col-2">
+                    <p class="font-weight-bold text-muted mb-1"><small>Átl. Benzink. / Cím</small></p>
                 </div>
             </div>
             @foreach($selectedReports as $report)
 
-                <div class="card card-body border-bottom border-secondary rounded-0 pr-5 pl-5 pt-0 pb-3 mb-4">
-                    <div class="row text-center">
-                        {{--dátum--}}
-                        <div class="col-2">
-                            <p class="h5 font-weight-bold">{{$report->created_at->translatedFormat('Y/m')}}</p>
-                        </div>
-                        {{--bevétel--}}
-                        <div class="col-2">
+                <div
+                    class="@if($selectedReports->last() != $report) border-bottom @else mb-2 @endif row text-center px-3 py-2">
+                    {{--dátum--}}
+                    <div class="col-2">
+                        <p class="h5 mb-0 font-weight-bold">{{$report->created_at->translatedFormat('Y/m')}}</p>
+                    </div>
+                    {{--bevétel--}}
+                    <div class="col-2">
+                        <p
+                            class="h5 font-weight-bold mb-0">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->gross_income) }}
+                            Ft</p>
+                        @if($report->hasPrevious())
                             <p
-                                class="h5 font-weight-bold mb-0">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->gross_income) }}
-                                Ft</p>
-                            @if($report->hasPrevious())
-                                <p
-                                    class="mb-0 @if($report->getIncomeDifference() > 0) text-success-pastel @else text-danger-pastel @endif">
-                                    <small class="font-weight-bold">{{ $report->getIncomeDifferencePercent() }}</small>
-                                </p>
-                            @endif
-                        </div>
-                        {{--kiadások--}}
-                        <div class="col-2">
+                                class="mb-0 @if($report->getIncomeDifference() > 0) text-success-pastel @else text-danger-pastel @endif">
+                                <small class="font-weight-bold">{{ $report->getIncomeDifferencePercent() }}</small>
+                            </p>
+                        @endif
+                    </div>
+                    {{--kiadások--}}
+                    <div class="col-2">
+                        <p
+                            class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->gross_expense) }}
+                            Ft</p>
+                        @if($report->hasPrevious())
                             <p
-                                class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->gross_expense) }}
-                                Ft</p>
-                            @if($report->hasPrevious())
-                                <p
-                                    class="mb-0 @if($report->getExpenseDifference() <= 0) text-success-pastel @else text-danger-pastel @endif">
-                                    <small class="font-weight-bold">{{ $report->getExpenseDifferencePercent() }}</small>
-                                </p>
-                            @endif
-                        </div>
-                        {{--kiszállítások--}}
-                        <div class="col-2">
+                                class="mb-0 @if($report->getExpenseDifference() <= 0) text-success-pastel @else text-danger-pastel @endif">
+                                <small class="font-weight-bold">{{ $report->getExpenseDifferencePercent() }}</small>
+                            </p>
+                        @endif
+                    </div>
+                    {{--kiszállítások--}}
+                    <div class="col-2">
+                        <p
+                            class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->delivered_orders) }}
+                            cím</p>
+                        @if($report->hasPrevious())
                             <p
-                                class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->delivered_orders) }}
-                                cím</p>
-                            @if($report->hasPrevious())
-                                <p
-                                    class="mb-0 @if($report->getDeliveriesDifference() > 0) text-success-pastel @else text-danger-pastel @endif">
-                                    <small class="font-weight-bold">{{ $report->getDeliveriesDifferencePercent() }}</small>
-                                </p>
-                            @endif
-                        </div>
-                        {{--bevétel címenként--}}
-                        <div class="col-2">
-                            <p
-                                class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->getAvgIncomeByDeliveries()) }}
-                                Ft</p>
-                        </div>
-                        {{--benzinköltség--}}
-                        <div class="col-2">
-                            <p
-                                class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->getDeliveryExpenseByAddress()) }}
-                                Ft</p>
-                        </div>
+                                class="mb-0 @if($report->getDeliveriesDifference() > 0) text-success-pastel @else text-danger-pastel @endif">
+                                <small
+                                    class="font-weight-bold">{{ $report->getDeliveriesDifferencePercent() }}</small>
+                            </p>
+                        @endif
+                    </div>
+                    {{--bevétel címenként--}}
+                    <div class="col-2">
+                        <p
+                            class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->getAvgIncomeByDeliveries()) }}
+                            Ft</p>
+                    </div>
+                    {{--benzinköltség--}}
+                    <div class="col-2">
+                        <p
+                            class="h5 mb-0 font-weight-bold">{{ resolve('App\Subesz\MoneyService')->getFormattedMoney($report->getDeliveryExpenseByAddress()) }}
+                            Ft</p>
                     </div>
                 </div>
             @endforeach
