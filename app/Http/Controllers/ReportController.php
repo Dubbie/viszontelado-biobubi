2df93bfb1f7bdf5a94c0752d90c18878ed0b29e3
Miho Daniel <dev.mihodaniel@gmail.com>
2021-04-13 12:03:41 +0200

    Fixes resellers not being homogenized

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Report;
 use App\ReportProducts;
 use App\Subesz\ReportService;
+use App\Subesz\UserService;
 use App\User;
 use Auth;
 use Carbon\Carbon;
@@ -17,6 +18,18 @@ use Schema;
 
 class ReportController extends Controller
 {
+    /** @var \App\Subesz\UserService */
+    private $userService;
+
+    /**
+     * ReportController constructor.
+     *
+     * @param  \App\Subesz\UserService  $userService
+     */
+    public function __construct(UserService $userService) {
+        $this->userService = $userService;
+    }
+
     /**
      * @return Factory|View
      */
@@ -63,12 +76,7 @@ class ReportController extends Controller
         /** @var User $reseller */
         /** @var ReportService $repService */
         $repService = resolve('App\Subesz\ReportService');
-        foreach (User::withCount('regions')->get() as $reseller) {
-            if ($reseller->regions_count == 0) {
-                Log::info('- %s nem viszonteladó, mivel nincs hozzárendelve régió, ezért kihagyjuk.');
-                continue;
-            }
-
+        foreach ($this->userService->getResellers() as $reseller) {
             $repService->generateReportByDate($reseller, Carbon::now());
         }
 
@@ -99,12 +107,7 @@ class ReportController extends Controller
             /** @var User $reseller */
             /** @var ReportService $repService */
             $repService = resolve('App\Subesz\ReportService');
-            foreach (User::withCount('zips')->get() as $reseller) {
-                if ($reseller->zips_count == 0) {
-                    Log::info(sprintf('- %s nem viszonteladó, mivel nincs hozzárendelve irányítószám, ezért kihagyjuk.', $reseller->name));
-                    continue;
-                }
-
+            foreach ($this->userService->getResellers() as $reseller) {
                 $repService->generateReportByDate($reseller, $rsDate);
             }
 
