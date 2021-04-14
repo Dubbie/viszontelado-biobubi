0e3f476711e74d7f80ef731b8ce246b667759cb4
Proksa Levente <proksalevente@gmail.com>
2021-04-14 18:30:02 +0200

    DRY the script, which changes between yearly and monthly reports

--- resources/views/inc/reseller-report-chooser.blade.php
+++ resources/views/inc/reseller-report-chooser.blade.php
@@ -4,3 +4,44 @@
 <div id="report-yearly-view" class="d-none">
     @include('inc.reseller-yearly-reports')
 </div>
+
+@section('scripts')
+    @parent
+    <script>
+
+        $(() => {
+            // Riport választó
+            let reportType = `{{$reportView}}`;
+            const monthlyCont = $("#report-monthly-view");
+            const monthlyBtn = $("#btn-monthly-view");
+            const yearlyCont = $("#report-yearly-view");
+            const yearlyBtn = $("#btn-yearly-view");
+
+            monthlyBtn.on('click', showMonthly);
+            yearlyBtn.on('click', showYearly);
+
+            function showMonthly() {
+                monthlyCont[0].classList.remove('d-none');
+                yearlyCont[0].classList.add('d-none');
+            }
+
+            function showYearly() {
+                yearlyCont[0].classList.remove('d-none');
+                monthlyCont[0].classList.add('d-none');
+            }
+
+            if (reportType == 'monthly-reports')
+                showMonthly();
+            if (reportType == 'yearly-reports')
+                showYearly();
+
+            //riport formok
+            $('#date').on('change', e => {
+                $(e.currentTarget).closest('form').submit();
+            });
+            $('#year').on('change', e => {
+                $(e.currentTarget).closest('form').submit();
+            });
+        });
+    </script>
+@stop
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -81,39 +81,6 @@
                     updateRsAddDynamicElements();
                 });
 
-                // Riport választó
-                let reportType = `{{$reportView}}`;
-                const monthlyCont = $("#report-monthly-view");
-                const monthlyBtn = $("#btn-monthly-view");
-                const yearlyCont = $("#report-yearly-view");
-                const yearlyBtn = $("#btn-yearly-view");
-
-                monthlyBtn.on('click', showMonthly);
-                yearlyBtn.on('click', showYearly);
-
-                function showMonthly() {
-                    monthlyCont[0].classList.remove('d-none');
-                    yearlyCont[0].classList.add('d-none');
-                }
-
-                function showYearly() {
-                    yearlyCont[0].classList.remove('d-none');
-                    monthlyCont[0].classList.add('d-none');
-                }
-
-                if (reportType == 'monthly-reports')
-                    showMonthly();
-                if (reportType == 'yearly-reports')
-                    showYearly();
-
-                //riport formok
-                $('#date').on('change', e => {
-                    $(e.currentTarget).closest('form').submit();
-                });
-                $('#year').on('change', e => {
-                    $(e.currentTarget).closest('form').submit();
-                });
-
                 $(document).on('click', '.btn-remove-cs-row', e => {
                     const btn = e.currentTarget;
                     const row = $(btn).closest('.cs-row')[0];
