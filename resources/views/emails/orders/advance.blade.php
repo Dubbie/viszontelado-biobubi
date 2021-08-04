606f6dd976c688784136b53edd68812d0be588a3
Mihó Dániel <dev.mihodaniel@gmail.com>
2021-08-04 16:47:34 +0200

    Fixes markdown

--- resources/views/emails/orders/advance.blade.php
+++ resources/views/emails/orders/advance.blade.php
@@ -2,13 +2,13 @@
     /** @var \App\Order $order */
 @endphp
 @component('mail::message')
-    # Megrendelés előleg értesítő
+# Megrendelés előleg értesítő
 
-    Kedves {{ $order->lastname }}!
+Kedves {{ $order->lastname }}!
 
-    Csatolva küldöm neked a megrendelésedhez tartozó előlegszámlát!<br>
-    Köszönöm, hogy nálunk vásároltál!<br>
+Csatolva küldöm neked a megrendelésedhez tartozó előlegszámlát!<br>
+Köszönöm, hogy nálunk vásároltál!<br>
 
 
-    **További szép napot neked!**
+**További szép napot neked!**
 @endcomponent
